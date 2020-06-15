f42e7428db797ebe2cb3a686e5c5cf92a1a58039
paulusnugroho123 <paulusagata123@gmail.com>
2020-06-15 15:16:00 +0700

    selesai yang pertama

---
+++ app/src/main/java/com/project/projek/MainActivity.java
@@ -0,0 +1,19 @@
+package com.project.projek;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
